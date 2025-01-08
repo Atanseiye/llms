c1f6da14007d695397f25dcdb8ce3c95ae0e8ec0
Kolade <koladeatanseiye@gmail.com>
2025-01-08 01:57:44 +0100

    test.py updated

--- tester.py
+++ tester.py
@@ -7,11 +7,3 @@ with open('the-verdict.txt', 'r') as file:
 
 
 
-v2 = TokenizerV2(vocabs(raw_text))
-token = v2.encode(raw_text)
-detoken = v2.decode(token)
-# print(detoken)
-
-# data = data_preparaion.prepare(token, context_size=5)
-
-print(token)
