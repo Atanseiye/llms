ccd31491c0426357a115d629f42d9b99b3ec50a3
Kolade <koladeatanseiye@gmail.com>
2025-01-09 14:08:49 +0100

    updating

--- embeddings.py
+++ embeddings.py
@@ -44,9 +44,13 @@ if __name__ == '__main__':
         raw_text, batch_size=12, max_length=max_lenght, stride=384,
         shuffle=False, drop_last=False, num_workers=0
         )
+    
     reatl_data = iter(datas)
     inputs, outputs = next(reatl_data)
     token_embeddings = embedding_layer(inputs)
     pos_embedding = pos_embedding_layer(torch.arange(max_lenght))
+    input_embedding = token_embeddings + pos_embedding
+
     print(f"token embeddings shape: {token_embeddings.shape}")
     print(f"Postional Embedding shape: {pos_embedding.shape}")
+    print(input_embedding)
