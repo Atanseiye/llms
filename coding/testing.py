f61d1a8afbf2c20160e3f18cb76f40b196e7dd18
Kolade <koladeatanseiye@gmail.com>
2025-01-11 10:36:29 +0100

    testing

---
+++ coding/testing.py
@@ -0,0 +1,15 @@
+import torch
+import matplotlib.pyplot as plt
+import mlp_toolkits.mplot3D as Axes3D
+
+inputs = torch.tensor(
+    [
+        [.43, .15, .89],
+        [.55, .87, .56],
+        [.57, .85, .64],
+        [.22, .58, .33],
+        [.77, .25, .10],
+        [.05, .80, .55]
+    ]
+)
+
