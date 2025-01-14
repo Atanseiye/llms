f3248ecb3bccdce5ac900918e2514b2f705e3f91
Kolade <koladeatanseiye@gmail.com>
2025-01-14 05:39:05 +0100

    causal attention

--- tester.py
+++ tester.py
@@ -15,8 +15,8 @@ inputs = torch.tensor(
 torch.manual_seed(123)
 batch = torch.stack((inputs, inputs), dim=0)
 context_lenght = batch.shape[1]
-d_in = batch.shape[0]
-d_out = batch.shape[2]
+d_in = batch.shape[2]
+d_out = batch.shape[0]
 ca = Attention(d_in, d_out, context_lenght, 0.0)
 context_vec = ca.causal(batch)
 print(context_vec)
