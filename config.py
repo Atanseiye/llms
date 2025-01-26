3b71bb99db7c7f376b2b2550a6a918e2044ca5c7
Kolade <koladeatanseiye@gmail.com>
2025-01-26 13:34:41 +0100

    config

---
+++ config.py
@@ -0,0 +1,9 @@
+YOR_GPT_CONFIG_124M = {
+    'vocab_size':50257,
+    'context_lenght':1024,
+    'emb_dim':768,
+    'n_heads':12,
+    'n_layers':12,
+    'frop_rate':0.01,
+    'qkv_bias':False
+}
