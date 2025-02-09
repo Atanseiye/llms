38bfa7dcaebb046fc58e7b12a10e2838c042ece3
Kolade <koladeatanseiye@gmail.com>
2025-02-09 14:57:45 +0100

    full transformer

---
+++ transformer.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+from layerNorm import LayerNorm
+from layers import Feedforward
+from config import YOR_GPT_CONFIG_124M
+from activation import GELU
+from attention import MultiHead
+
+class TransformerBlock(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.att = MultiHead(
+            d_in=config['emb_dim'],
+            d_out=config['emb_dim'],
+            context_lenght=config['context_lenght'],
+            num_head=config['n_heads'],
+            dropout=config['drop_rate'],
+            qvk_bais=config['qkv_bias']
+        )
+        self.ff = Feedforward(config)
+        self.norm1 = LayerNorm(config['emb_dim'])
+        self.norm2 = LayerNorm(config['emb_dim'])
+        self.drop_shortcut = nn.Dropout(config['drop_rate'])
+
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut #Add original inout back
+
+        # Shortcut connection for the Feedforward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut # Add original inout back
+        
+        return x
