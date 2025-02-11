2cde89a65de83863d353e6cfb5e602bcb5ada72f
Kolade <koladeatanseiye@gmail.com>
2025-02-11 20:23:06 +0100

    put layer normalization class in the layer module

--- layers.py
+++ layers.py
@@ -1,8 +1,10 @@
 from config import YOR_GPT_CONFIG_124M
 from util import GELU
 from torch import nn
+import torch
 
-# Feedforward Neural Network
+#           Feedforward Neural Network
+# ====================================================
 class Feedforward(nn.Module):
 
     def __init__(self, config):
@@ -17,5 +19,17 @@ class Feedforward(nn.Module):
         return self.layer(x)
     
 
+#            Layer Normarlization Module
+# ====================================================
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
 
-
+    def forward(self, x):
+        mean = x.mean(dim=1, keepdim=True)
+        var = x.var(dim=1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
