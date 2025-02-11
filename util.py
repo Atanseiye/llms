c6c28e3ac4bdb19b96a3d1ddfe29d03c6c20ed00
Kolade <koladeatanseiye@gmail.com>
2025-02-11 20:15:23 +0100

    activation function was added util modules

--- util.py
+++ util.py
@@ -1,1 +1,14 @@
-ECHO is on.
+import torch
+from torch import nn
+
+class GELU(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2 / torch.pi)) * 
+            (x + .044715 * torch.pow(x, 3))
+            ))
+    
